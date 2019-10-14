31085063cc43c1f7308a4c78c624ea06be397fd5
Arthur Smith <arthurpsmith@gmail.com>
2019-10-14 10:20:56 -0400

    Replace hard-wired "oauth.ini" file location with variable.

--- work_item_oauth.php
+++ work_item_oauth.php
@@ -3,7 +3,7 @@
 require_once ( __DIR__ . '/lib/initialize.php' ) ;
 require_once ( __DIR__ . '/lib/wikidata_oauth.php' );
 
-$oauth = new WD_OAuth('author-disambiguator', '/var/www/html/oauth.ini');
+$oauth = new WD_OAuth('author-disambiguator', $oauth_ini_file);
 
 $action = get_request ( 'action' , '' ) ;
 $work_qid = get_request( 'id', '' ) ;
