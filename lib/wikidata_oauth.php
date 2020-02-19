2780740c415a091f0d0ec5966ae5d1c3c58920a3
Arthur Smith <arthurpsmith@gmail.com>
2020-02-19 08:47:02 -0500

    Add "interactive" mode

--- lib/wikidata_oauth.php
+++ lib/wikidata_oauth.php
@@ -17,6 +17,7 @@ class WD_OAuth {
 	var $delay_after_create_s = 2 ;
 	var $delay_after_edit_s = 1 ;
 	var $delay_after_upload_s = 1 ;
+	var $interactive = false ;
 	
 	function __construct ( $tool, $ini_file, $token_key='', $token_secret='' ) {
 		$this->tool = $tool ;
@@ -401,15 +402,13 @@ class WD_OAuth {
 	 * @return array API results
 	 */
 	function doApiQuery( $post, &$ch = null , $mode = '' , $iterations_left = 12 , $last_maxlag = -1 ) {
-		if ( $iterations_left <= 0 ) return ; // Avoid infinite recursion when Wikidata Is Too Damn Slow Again
-
 		global $maxlag ;
 		if ( !isset($maxlag) ) $maxlag = 5 ;
 		$give_maxlag = $maxlag ;
 		if ( $last_maxlag != -1 ) $give_maxlag = $last_maxlag ;
 
 		// Not an edit, high maxlag allowed
-		if ( isset($post['action']) and $post['action']=='query' and isset($post['meta']) and $post['meta']=='userinfo' ) {
+		if ( isset($post['action']) and $post['action'] !='wbeditentity' ) {
 			$give_maxlag = 99999 ;
 		}
 
@@ -492,7 +491,10 @@ class WD_OAuth {
 		
 		# maxlag
 		if ( isset($ret->error) and isset($ret->error->code) and $ret->error->code == 'maxlag' ) {
+			if ( $iterations_left <= 1 ) return $ret ;
+
 			$lag = $maxlag ;
+
 			sleep ( 60*$lag ) ; # Give it plenty of time before retrying 
 			$ch = null ;
 			$ret = $this->doApiQuery( $post, $ch , '' , $iterations_left-1 , $last_maxlag ) ;
@@ -564,7 +566,7 @@ class WD_OAuth {
 		return $item;
 	}
 
-	function apply_commands_to_item($qid, $baserev, $edit_summary, $token, $ch, $commands) {
+	function apply_commands_to_item($qid, $baserev, $edit_summary, $token, $ch, $commands ) {
 		$data = array();
 		$data['claims'] = $commands;
 		$params = [
@@ -577,7 +579,9 @@ class WD_OAuth {
 		if ( isset ( $baserev ) and $baserev != '' ) $params['baserevid'] = $baserev ;
 		if ( isset($edit_summary) and $edit_summary != '' ) $params['summary'] = $edit_summary ;
 
-		$res = $this->doApiQuery( $params , $ch );
+		$retries = $this->interactive ? 1 : 20 ;
+
+		$res = $this->doApiQuery( $params , $ch, '', $retries );
 		if ( isset ( $res->error ) ) {
 			$this->error = "Error code: " . $res->error->code ;
 			return false ;
