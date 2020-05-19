b1cc3c92f434010bd68129280057fe8de1072f03
Arthur Smith <arthurpsmith@gmail.com>
2020-05-19 10:43:28 -0400

    Use label fetcher for authors and other items needed

--- names_oauth.php
+++ names_oauth.php
@@ -217,15 +217,16 @@ print "<form method='post' class='form' target='_blank' action='?'>
 <input type='hidden' name='wbsearch' value='$wbsearch' />
 <input type='hidden' name='name' value='" . escape_attribute($name) . "' />" ;
 
-$to_load = array() ;
 $article_items = generate_article_entries( $items_papers );
+
+# Just need labels for the following:
+$qids_to_label = array();
 foreach ( $article_items AS $article ) {
-	foreach ( $article->authors AS $auth) $to_load[] = $auth ;
-	foreach ( $article->published_in AS $pub ) $to_load[] = $pub ;
-	foreach ( $article->topics AS $topic ) $to_load[] = $topic ;
+	foreach ( $article->authors AS $auth) $qids_to_label[$auth] = 1 ;
+	foreach ( $article->published_in AS $pub ) $qids_to_label[$pub] = 1 ;
+	foreach ( $article->topics AS $topic ) $qids_to_label[$topic] = 1 ;
 }
-$to_load = array_unique( $to_load );
-$wil->loadItems ( $to_load ) ;
+$qid_labels = AuthorData::labelsForItems(array_keys($qids_to_label));
 
 #print "<pre>" ; print_r ( $article_items) ; print "</pre>" ;
 
@@ -324,9 +325,7 @@ foreach ( $clusters AS $cluster_name => $cluster ) {
 			if (isset($formatted_authors[$num])) {
 				$display_num = "$num-$qt";
 			}
-			$i2 = $wil->getItem ( $qt ) ;
-			if ( !isset($i2) ) continue ;
-			$label = $i2->getLabel() ;
+			$label = $qid_labels[$qt];
 			$author_qid_counter[$qt] = isset($author_qid_counter[$qt]) ? $author_qid_counter[$qt]+1 : 1 ;
 			$formatted_authors[$display_num] = "[$display_num]<a href='author_item_oauth.php?id=$qt' target='_blank' style='color:green'>$label</a>" ;
 		}
@@ -335,10 +334,9 @@ foreach ( $clusters AS $cluster_name => $cluster ) {
 
 		$published_in = array() ;
 		foreach ( $article->published_in AS $qt ) {
-			$i2 = $wil->getItem ( $qt ) ;
-			if ( !isset($i2) ) continue ;
+			$label = $qid_labels[$qt];
 			$venue_counter[$qt] = isset($venue_counter[$qt]) ? $venue_counter[$qt]+1 : 1 ;
-			$published_in[] = wikidata_link($i2->getQ(), $i2->getLabel(), 'black') . "&nbsp;[<a href='https://tools.wmflabs.org/scholia/venue/" . $i2->getQ() . "/missing' target='_blank'>missing</a>]" ;
+			$published_in[] = wikidata_link($qt, $label, 'black') . "&nbsp;[<a href='https://tools.wmflabs.org/scholia/venue/$qt/missing' target='_blank'>missing</a>]" ;
 		}
 		$published_in_list = implode ( ', ', $published_in ) ;
 	
@@ -360,10 +358,9 @@ foreach ( $clusters AS $cluster_name => $cluster ) {
 		if ( count($article->topics) > 0 ) {
 			$topics = [] ;
 			foreach ( $article->topics AS $qt ) {
-				$i2 = $wil->getItem($qt) ;
-				if ( !isset($i2) ) continue ;
+				$label = $qid_labels[$qt];
 				$topic_counter[$qt] = isset($topic_counter[$qt]) ? $topic_counter[$qt]+1 : 1 ;
-				$topics[] = wikidata_link($i2->getQ(), $i2->getLabel(), 'brown') . "&nbsp;[<a href='https://tools.wmflabs.org/scholia/topic/" . $i2->getQ() . "/missing' target='_blank'>missing</a>]" ;
+				$topics[] = wikidata_link($qt, $label, 'brown') . "&nbsp;[<a href='https://tools.wmflabs.org/scholia/topic/$qt/missing' target='_blank'>missing</a>]" ;
 			}
 			print implode ( '; ' , $topics ) ;
 		}
@@ -450,8 +447,7 @@ arsort ( $author_qid_counter, SORT_NUMERIC ) ;
 print "<h2>Author items in these papers</h2>" ;
 print "<ul>" ;
 foreach ( $author_qid_counter AS $qt => $cnt ) {
-	$i2 = $wil->getItem($qt) ;
-	$label = $i2->getLabel() ;
+	$label = $qid_labels[$qt];
 	print "<li><a href='author_item_oauth.php?limit=50&id=$qt' style='color:green'>$label</a> (<a href='?fuzzy=$fuzzy&wbsearch=$wbsearch&limit=$article_limit&name=" . urlencode($name) . "&filter=wdt%3AP50+wd%3A$qt'>$cnt&times;</a>)</li>";
 }
 print "</ul>" ;
@@ -469,9 +465,8 @@ arsort ( $venue_counter , SORT_NUMERIC ) ;
 print "<h2>Publishing venues for these papers</h2>" ;
 print "<ul>" ;
 foreach ( $venue_counter AS $qt => $cnt ) {
-	$i2 = $wil->getItem($qt) ;
-	$label = $i2->getLabel() ;
-	print "<li>" . wikidata_link($i2->getQ(), $i2->getLabel(), 'black') . " (<a href='?fuzzy=$fuzzy&wbsearch=$wbsearch&limit=$article_limit&name=" . urlencode($name) . "&filter=wdt%3AP1433+wd%3A$qt'>$cnt&times;</a>)</li>" ;
+	$label = $qid_labels[$qt];
+	print "<li>" . wikidata_link($qt, $label, 'black') . " (<a href='?fuzzy=$fuzzy&wbsearch=$wbsearch&limit=$article_limit&name=" . urlencode($name) . "&filter=wdt%3AP1433+wd%3A$qt'>$cnt&times;</a>)</li>" ;
 }
 print "</ul>" ;
 
@@ -479,9 +474,8 @@ arsort ( $topic_counter , SORT_NUMERIC ) ;
 print "<h2>Topics for these papers</h2>" ;
 print "<ul>" ;
 foreach ( $topic_counter AS $qt => $cnt ) {
-	$i2 = $wil->getItem($qt) ;
-	$label = $i2->getLabel() ;
-	print "<li>" . wikidata_link($i2->getQ(), $i2->getLabel(), 'brown') . " (<a href='?fuzzy=$fuzzy&wbsearch=$wbsearch&limit=$article_limit&name=" . urlencode($name) . "&filter=wdt%3AP921+wd%3A$qt'>$cnt&times;</a>)</li>" ;
+	$label = $qid_labels[$qt];
+	print "<li>" . wikidata_link($qt, $label, 'brown') . " (<a href='?fuzzy=$fuzzy&wbsearch=$wbsearch&limit=$article_limit&name=" . urlencode($name) . "&filter=wdt%3AP921+wd%3A$qt'>$cnt&times;</a>)</li>" ;
 }
 print "</ul>" ;
 
