9809855d6eb0aa46fb9ce7562c315d0b57e2cea6
Arthur Smith <arthurpsmith@gmail.com>
2021-05-21 10:48:06 -0400

    Add sample files and update README to allow others to run this

---
+++ sample_local_config.php
@@ -0,0 +1,7 @@
+<?PHP
+
+  $oauth_ini_file = 'FULL_PATH_LOCATION_OF_OAUTH_FILE';
+  $oauth_url_prefix = 'https://MY_HOST_NAME/author-disambiguator/';
+  $db_passwd_file = 'FULL_PATH_LOCATION_OF_DB_FILE';
+
+?>
